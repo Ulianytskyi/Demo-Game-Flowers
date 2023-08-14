606b27c8acaa56846900643657cfb7625fcf4fa8
Artem Ulianytskyi <darthdeir@gmail.com>
2023-08-14 19:57:01 +0300

    Commit 1408

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-const container = document.querySelector(".container");
-const splashScreen = document.querySelector(".splash-screen");
-const finalText = document.getElementById("final-text");
-const finalCollected = document.getElementById("final-collected");
-const finalScore = document.getElementById("final-score");
-const currScore = document.getElementById("score");
-const timer = document.getElementById("timer");
-const collectedItems = document.getElementById("collected-items");
-const gameGoal = document.getElementById("goal-text");
+const container = document.querySelector('.container');
+const splashScreen = document.querySelector('.splash-screen');
+const goalScreen = document.querySelector('.goal-screen');
+const finalBlock = document.querySelector('.final-block');
+const finalText = document.getElementById('final-text');
+const winLoseStyle = document.querySelector('.win-lose');
+const finalCollected = document.getElementById('final-collected');
+const finalScore = document.getElementById('final-score');
+const currScore = document.getElementById('score');
+const healthPoints = document.getElementById('health');
+const collectedItems = document.getElementById('collected-items');
+
+const gameField = document.getElementById("game-field");
+const gameGoal = document.getElementById('goal-text');
 
 let score = 0;
 let collected = 0;
-let goal = "";
-
 let array1 = [];
 let array2 = [];
-const gameField = document.getElementById("game-field");
-// displayField(array1, gameField);
+let goal = '';
+let health = 0;
 
-function makeGoal() {
-  goal = flowerType(getRandomNumber(1, 3));
-  gameGoal.innerHTML = "Goal: " + goal;
-  collectedItems.innerHTML = `Collected: ${goal} ${collected}`;
-}
 
 function generateArray(length, min, max) {
   const tempArray = [];
@@ -39,7 +37,7 @@ function getRandomNumber(min, max) {
 }
 
 function flowerType(number) {
-  const flowerIcons = ["🌱", "🌼", "🌻", "🌷"];
+  const flowerIcons = ["рџЊ±", "рџЊј", "рџЊ»", "рџЊ·"];
   return flowerIcons[number];
 }
 
@@ -57,44 +55,51 @@ function displayObject(array, index, targetElement) {
   let objectBtnId = "button" + index;
 
   targetElement.innerHTML += `<div class = '${objectClass}'>
-  <button class='${objectBtnClass} ${objectIconClass}' id='${objectBtnId}'>
-  ${array[index].icon}
-  </button>
-  </div>`;
+      <button class='${objectBtnClass} ${objectIconClass}' id='${objectBtnId}'>
+      ${array[index].icon}
+      </button>
+    </div>`;
 }
 
 function checkGoal(goal) {
   const targetName = goal;
   const objectsWithTargetName = array1.filter((obj) => obj.icon === targetName);
-  if (objectsWithTargetName.length > 25) {
-    console.log(`You win!`);
+  if (objectsWithTargetName.length == 0) {
+    finalText.innerHTML = 'You win!';
+    winLoseStyle.classList.add('win-style');
     checkGameOver();
   }
 }
 
-function disableAllButtons() {
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.disabled = true;
-  });
+function checkHealth() {
+  switch (health) {
+    case 1:
+      healthPoints.innerHTML = 'вќ¤пёЏрџ¤Ќрџ¤Ќ';
+      break;
+    case 2:
+      healthPoints.innerHTML = 'вќ¤пёЏвќ¤пёЏрџ¤Ќ';
+      break;
+    case 3:
+      healthPoints.innerHTML = 'вќ¤пёЏвќ¤пёЏвќ¤пёЏ';
+      break;
+  }
+  if (health == 0) {
+    finalText.innerHTML = 'You lose!';
+    winLoseStyle.classList.remove('win-style');
+    checkGameOver();
+  }
 }
 
 function checkGameOver() {
-  splashScreen.classList.remove("hide");
-  container.classList.add("hide");
-  finalText.classList.remove("hide");
-  finalCollected.classList.remove("hide");
-  finalScore.classList.remove("hide");
-  gameGoal.classList.add("hide");
+  splashScreen.classList.remove('hide');
+  container.classList.add('hide');
+  finalText.classList.remove('hide');
+  finalCollected.classList.remove('hide');
+  finalScore.classList.remove('hide');
   finalCollected.innerText = `Collected: ${goal} ${collected}`;
   finalScore.innerText = `Score: ${score}`;
   startButton.innerText = "Restart Game";
 
-  array1 = generateArray(105, 1, 3);
-  array2 = [];
-  score = 0;
-  currScore.innerHTML = `Score: ${score}`;
-  displayField(array1, gameField);
 }
 
 function update() {
@@ -102,52 +107,78 @@ function update() {
 
   buttons.forEach((button, index) => {
     button.addEventListener("click", function () {
-      if (array2.length < 3) {
-        array2.push(index);
-      } else {
-        const firstIndex = array2.shift();
-        array1[firstIndex].icon = flowerType(getRandomNumber(1, 3));
-        buttons[firstIndex].textContent = array1[firstIndex].icon;
-        buttons[firstIndex].disabled = false;
-        array2.push(index);
+      if (array1[index] && array1[index].icon) {
+        if (array2.length < 3) {
+          array2.push(index);
+          
+        } else {
+          const firstIndex = array2.shift();
+          array1[firstIndex].icon = flowerType(getRandomNumber(1, 3));
+          buttons[firstIndex].textContent = array1[firstIndex].icon;
+          buttons[firstIndex].disabled = false;
+          array2.push(index);
+        }
+
+        if (array1[index].icon == goal) {
+          collected++;
+          score += (health * 3);
+        } else {
+          health -= 1;
+          score -= 9;
+          if (score < 0) {
+            score = 0;
+          }
+        }
+
+        console.log('health ' + health)
+
+        currScore.innerHTML = `Score: ${score}`;
+        collectedItems.innerHTML = `Collected: ${goal} ${collected}`
+
+        array1[index].icon = flowerType(0);
+        button.textContent = flowerType(0);
+        button.disabled = true;
+  
+        checkHealth();
+        checkGoal(goal);
       }
-
-      console.log(array1[index].icon);
-
-      if (array1[index].icon == goal) {
-        collected++;
-        score += 10;
-      } else {
-        score -= 10;
-      }
-      currScore.innerHTML = `Score: ${score}`;
-      collectedItems.innerHTML = `Collected: ${goal} ${collected}`;
-
-      array1[index].icon = flowerType(0);
-      button.textContent = flowerType(0);
-      button.disabled = true;
-
-      checkGoal(goal);
     });
   });
+  
+}
+
+function initialization() {
+  score = 0;
+  collected = 0;
+  array1 = generateArray(98, 1, 3);
+  array2 = [];
+  health = 3;
+  displayField(array1, gameField);
+  goal = flowerType(getRandomNumber(1, 3));
+  gameGoal.innerHTML = "Goal: " + goal;
+  collectedItems.innerHTML = `Collected: ${goal} ${collected}`;
+  currScore.innerHTML = `Score: ${score}`;
+  checkHealth();
+  console.log(health);
 }
 
 function startGame() {
-  array1 = generateArray(105, 1, 3);
-  array2 = [];
-  displayField(array1, gameField);
-  if (goal != "") {
-    makeGoal();
-  }
-  container.classList.remove("hide");
-  splashScreen.classList.add("hide");
+  container.classList.remove('hide');
+  goalScreen.classList.add('hide');
+  initialization();
   update();
 }
 
-const startButton = document.getElementById("start-button");
-startButton.addEventListener("click", function () {
-  startGame();
-  document.body.style.overflow = "inherit";
+const startButton = document.getElementById('start-button');
+startButton.addEventListener('click', function() {
+  splashScreen.classList.add('hide');
+  finalBlock.classList.remove('hide');
+  goalScreen.classList.remove('hide');
+  document.body.style.overflow = 'inherit';
 });
 
-makeGoal();
+const startGameButton = document.getElementById('start-game');
+startGameButton.addEventListener('click', function() {
+  startGame();
+  document.body.style.overflow = 'inherit';
+});
